1d83b2593a92a41435ac1462ca75e92c9212f7aa
Nikolai Sakovich <kolsak2013@yandex.ru>
2023-12-01 18:23:34 +0300

    Initial commit

---
+++ src/test/java/com/example/springboothomework27/SpringBootHomework27ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboothomework27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHomework27ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
